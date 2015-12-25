8c3109aee79f7aace04b5d695294dcd9387a2c96
Enieber <eniebercunha@gmail.com>
2015-12-24 23:07:43 -0200

    add new test

--- test/empresa.spec.js
+++ test/empresa.spec.js
@@ -2,12 +2,21 @@
 
 exports.empresa = function(baseUrl,request,expect) {
   describe('when requested at /empresa', ()=> {
-    it('test url /empresa', (done) => {
+
+    it('test valid URL /empresa', (done) => {
       request.get(baseUrl+'/empresa').end(function assert(err, res){
         expect(err).to.not.be.ok;
         expect(res).to.have.property('status', 200);
         done();
       });
     });
+
+    it('test invalid URL /empresas', (done) => {
+      request.get(baseUrl+'/empresas').end(function assert(err, res) {
+        expect(res).to.have.property('status', 404);
+        done();
+      });
+    });
+
   });
 }
