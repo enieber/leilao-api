e3e79e9c0344638ae3cf357dafd443111e20777c
Enieber <eniebercunha@gmail.com>
2015-12-26 01:33:16 -0200

    refectory test

--- test/empresa.spec.js
+++ test/empresa.spec.js
@@ -1,13 +1,20 @@
 "use strict";
 
 exports.empresa = function(baseUrl,request,expect) {
-  describe('when requested at /empresa', ()=> {
-
+  describe('Requests for empresa', ()=> {
 
     it('test valid URL /empresa', (done) => {
       request.get(baseUrl+'/empresa').end(function assert(err, res){
         expect(err).to.not.be.ok;
         expect(res).to.have.property('status', 200);
+        done();
+      });
+    });
+
+    it('test bad request /empresa/abc', (done) => {
+      request.get(baseUrl+'/empresa/abc').end(function assert(err, res) {
+        expect(err).to.be.ok;
+        expect(res).to.have.property('status', 400);
         done();
       });
     });
@@ -20,43 +27,25 @@ exports.empresa = function(baseUrl,request,expect) {
       });
     });
 
-    it('test invalid URL /empresasss', (done) => {
-      request.get(baseUrl+'/empresasss').end(function assert(err, res) {
-        expect(err).to.be.ok;
-        expect(res).to.have.property('status', 404);
-        done();
-      });
-    });
-
-    it('test invalid URL /empresa/abc', (done) => {
-      request.get(baseUrl+'/empresa/abc').end(function assert(err, res) {
-        expect(err).to.be.ok;
-        expect(res).to.have.property('status', 400);
-        done();
-      });
-    });
-
     it('test post /empresa', (done) => {
       request.post(baseUrl+'/empresa')
         .send({
-      idEmpresa: 4,
-      idLeilao: 4,
-      cnpj: '12.345.678./0002-96',
-      razaoSocial: 'Motivados por um mundo melhor',
-      usuario: 'joao',
-      senha: '123JMJ123',
-      email: 'joaotest@jmj.com.br',
-      telefone: 6652515451,
-      logradouro: 'Portoario',
-      municipio: 'Sorriso',
-      numero: 10,
-      bairro: 'Centro',
-      cep: 78588888
-      }).end(function assert(err, res) {
-        expect(err).to.not.be.ok;
-        expect(res).to.have.property('status', 200);
+          cnpj: '12.345.678./0002-96',
+          razaoSocial: 'Motivados por um mundo melhor',
+          usuario: 'joao',
+          senha: '123JMJ123',
+          email: 'joaotest@jmj.com.br',
+          telefone: 6652515451,
+          logradouro: 'Portoario',
+          municipio: 'Sorriso',
+          numero: 10,
+          bairro: 'Centro',
+          cep: 78588888
+        }).end(function assert(err, res) {
+          expect(err).to.not.be.ok;
+          expect(res).to.have.property('status', 200);
 
-        done();
+          done();
       });
     });
 
--- test/index.spec.js
+++ test/index.spec.js
@@ -9,6 +9,15 @@ const loteTeste = require('./lote.spec');
 
 describe('Api leilao', function (){
   let baseUrl = "http://localhost:3000";
+  describe('test server', function (){
+    it('test server running', (done) => {
+      request.get(baseUrl+'/test').end(function assert(err, res){
+        expect(err).to.not.be.ok;
+        expect(res).to.have.property('status', 200);
+        done();
+      });
+    });
+  });
   empresaTeste.empresa(baseUrl,request,expect);
   // compradorTeste.comprador(baseUrl,request,expect);
   // leilaoTeste.leilao(baseUrl,request,expect);
