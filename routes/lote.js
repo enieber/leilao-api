453edd0bff755b7be6bcd55789e59a1b1613d7dc
Enieber <eniebercunha@gmail.com>
2015-12-27 15:38:37 -0200

    error on second test lotes

--- routes/lote.js
+++ routes/lote.js
@@ -29,7 +29,7 @@ exports.route = (server) => {
                       .number()
                       .integer()
                       .required()
-                }
+              }
             }
           }
         }
--- test/lote.spec.js
+++ test/lote.spec.js
@@ -11,14 +11,6 @@ exports.lote = function(baseUrl,request,expect) {
       });
     });
 
-    it('test valid URL /empresa/1/leilao/1/lote', (done) => {
-      request.get(baseUrl+'/empresa/1/leilao/1/lote').end(function assert(err, res){
-        expect(err).to.not.be.ok;
-        expect(res).to.have.property('status', 200);
-        done();
-      });
-    });
-
     it('test bad request /empresa/1/leilao/abc/lote', (done) => {
       request.get(baseUrl+'/empresa/1/leilao/abc/lote').end(function assert(err, res) {
         expect(err).to.be.ok;
@@ -27,5 +19,12 @@ exports.lote = function(baseUrl,request,expect) {
       });
     });
 
+    it('test valid URL /empresa/1/leilao/1/lote', (done) => {
+      request.get(baseUrl+'/empresa/1/leilao/1/lote').end(function assert(err, res){
+        expect(err).to.not.be.ok;
+        expect(res).to.have.property('status', 200);
+        done();
+      });
+    });
   });
 };
