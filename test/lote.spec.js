668e7918cfb96b1b35680ea3cee3a109ee60d23f
Enieber <eniebercunha@gmail.com>
2015-12-27 15:39:36 -0200

    fix error test lotes

--- test/lote.spec.js
+++ test/lote.spec.js
@@ -3,8 +3,8 @@
 exports.lote = function(baseUrl,request,expect) {
   describe('when request at lotes', () => {
 
-    it('test url /lotes ', () => {
-      request.get(baseUrl+'/lotes').end(function assert(err, res){
+    it('test url /lote', () => {
+      request.get(baseUrl+'/lote').end(function assert(err, res){
         expect(err).to.not.be.ok;
         expect(res).to.have.property('status', 200);
         done();
