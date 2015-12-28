233dd0d3f1f79936fa22baf47b026911a91badc9
Enieber <eniebercunha@gmail.com>
2015-12-28 01:36:39 -0200

    migration for fouder server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const Hapi = require('hapi');
-const models = require('../models');
-const empresa = require('../routes/empresa');
-const leilao = require('../routes/leilao');
-const lote = require('../routes/lote');
-const comprador = require('../routes/comprador');
-const test = require('../routes/index');
+const models = require('./models');
+const empresa = require('./routes/empresa');
+const leilao = require('./routes/leilao');
+const lote = require('./routes/lote');
+const comprador = require('./routes/comprador');
+const test = require('./routes/index');
 const Inert = require('inert');
 const path = require('path');
 const server = new Hapi.Server();
