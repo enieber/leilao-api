a43237e69f53a2649f377e781b1964205769437e
Progamador <jmj-prog@gmail.com>
2015-12-28 11:48:11 -0200

    add index test

---
+++ test/index.spec.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const expect = require('chai').expect;
+const request = require('superagent');
+const empresaTeste = require('./gets/empresa.spec.js');
+const compradorTeste = require('./gets/comprador.spec');
+const leilaoTeste = require('./gets/leilao.spec');
+const loteTeste = require('./gets/lote.spec');
+const connection = require('./gets/index.spec');
+const posts = require('./post.spec');
+const deletes = require('./delete.spec');
+const baseUrl = "http://localhost:3000";
+
+describe('Api leilao', function (){
+  connection.test(baseUrl, request, expect);
+  empresaTeste.empresa(baseUrl,request,expect);
+  leilaoTeste.leilao(baseUrl,request,expect);
+  loteTeste.lote(baseUrl,request,expect);
+  compradorTeste.comprador(baseUrl,request,expect);
+  posts.create(baseUrl,request,expect);
+});
